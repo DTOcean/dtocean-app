0e10f4758bbd26bbe3c6b5769b74abfef9636917
Mathew Topper <damm_horse@yahoo.co.uk>
2017-04-02 16:43:54 +0100

    Add WEC Simulator as an external tool

---
+++ dtocean_app/tools/external.py
@@ -0,0 +1,52 @@
+
+#    Copyright (C) 2016 Mathew Topper, Rui Duarte
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+# Set up logging
+import logging
+
+module_logger = logging.getLogger(__name__)
+
+from dtocean_core.tools.external import WECSimulatorTool
+
+from . import GUITool
+
+
+class GUIWECSimulatorTool(GUITool, WECSimulatorTool):
+    
+    """A basic strategy which will run all selected modules and themes in
+    sequence."""
+    
+    def __init__(self):
+        
+        WECSimulatorTool.__init__(self)
+        GUITool.__init__(self)
+        
+        return
+    
+    def get_weight(self):
+
+        '''A method for getting the order of priority of the strategy.
+
+        Returns:
+          int
+        '''
+
+        return 1
+    
+    def has_widget(self):
+
+        return False
+
