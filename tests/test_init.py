84483d6d8f4b8a480d2bcadeb9b762ff5ab384a0
Mathew Topper <damm_horse@yahoo.co.uk>
2017-05-31 10:55:22 +0100

    Add tests for init functions.

---
+++ tests/test_init.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr 13 15:16:37 2017
+
+@author: mtopper
+"""
+
+import warnings
+
+from dtocean_app import warn_with_traceback, start_logging
+
+
+def test_warn_with_traceback():
+    
+    warnings.showwarning = warn_with_traceback
+    warnings.warn("Test warning")
+    
+    assert True
+
+
+def test_start_logging():
+    
+    start_logging()
+    
+    assert True
