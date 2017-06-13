a289e695031a12a97d27a751c05f0b74b2b7e54c
Mathew Topper <damm_horse@yahoo.co.uk>
2017-06-13 14:08:19 +0100

    Add init_config function to copy config files to user data directory and call as a post-install script in setup.py

--- tests/test_configure.py
+++ tests/test_configure.py
@@ -1,5 +1,20 @@
 
-from dtocean_app import start_logging
+from polite.paths import Directory
+from dtocean_app import init_config, start_logging
+
+
+def test_init_config(mocker, tmpdir):
+
+    # Make a source directory with some files
+    config_tmpdir = tmpdir.mkdir("config")
+    mock_dir = Directory(str(config_tmpdir))
+        
+    mocker.patch('dtocean_app.UserDataDirectory',
+                 return_value=mock_dir)
+                 
+    init_config()
+        
+    assert len(config_tmpdir.listdir()) == 2
 
     
 def test_start_logging():
@@ -7,4 +22,3 @@ def test_start_logging():
     start_logging(True)
     
     assert True
-
