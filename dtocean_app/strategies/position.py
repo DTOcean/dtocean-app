410ccc3b723a34803c85c3ee3773b0fce31d245d
Mathew Topper <damm_horse@yahoo.co.uk>
2019-08-17 13:36:58 +0100

    Add active project modules and worker directory status to report.
    Add implementation of all controls in Control tab, apart from the auto threads option.

--- dtocean_app/strategies/position.py
+++ dtocean_app/strategies/position.py
@@ -15,7 +15,9 @@
 #    You should have received a copy of the GNU General Public License
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+import os
 import multiprocessing
+from copy import deepcopy
 
 from PyQt4 import QtCore, QtGui
 
@@ -71,6 +73,57 @@ class GUIAdvancedPosition(GUIStrategy, AdvancedPosition):
         widget = AdvancedPositionWidget(parent, shell, config)
         
         return widget
+    
+    @classmethod
+    def get_worker_directory_status(cls, config):
+        
+        root_project_path = config['root_project_path']
+        worker_directory = config["worker_dir"]
+        
+        status_strs = []
+        status_codes = []
+        
+        if os.path.isdir(worker_directory):
+            
+            if len(os.listdir(worker_directory)) == 0:
+                
+                status_str = "Worker directory empty"
+                status_code = 1
+                
+                status_strs.append(status_str)
+                status_codes.append(status_code)
+            
+            elif not config['clean_existing_dir']:
+                
+                status_str = "Worker directory contains files"
+                status_code = 0
+                
+                status_strs.append(status_str)
+                status_codes.append(status_code)
+            
+            _, root_project_name = os.path.split(root_project_path)
+            root_project_base_name, _ = os.path.splitext(root_project_name)
+            pickle_name = "{}_results.pkl".format(root_project_base_name)
+            
+            results_path = os.path.join(worker_directory, pickle_name)
+            
+            if os.path.isfile(results_path):
+                
+                status_str = "Optimisation complete"
+                status_code = 1
+                
+                status_strs.append(status_str)
+                status_codes.append(status_code)
+        
+        else:
+            
+            status_str = "Worker directory does not yet exist"
+            status_code = 1
+            
+            status_strs.append(status_str)
+            status_codes.append(status_code)
+        
+        return status_strs, status_codes
 
 
 class AdvancedPositionWidget(QtGui.QWidget,
@@ -82,12 +135,13 @@ class AdvancedPositionWidget(QtGui.QWidget,
     config_set = QtCore.pyqtSignal()
     config_null = QtCore.pyqtSignal()
     
-    def __init__(self, parent, config):
+    def __init__(self, parent, shell, config):
         
         QtGui.QWidget.__init__(self, parent)
         Ui_AdvancedPositionWidget.__init__(self)
         StrategyWidget.__init__(self)
         
+        self._shell = shell
         self._config = self._init_config(config)
         self._max_threads = multiprocessing.cpu_count()
         self._init_ui()
@@ -109,43 +163,105 @@ class AdvancedPositionWidget(QtGui.QWidget,
         self.setupUi(self)
         
         self.nThreadSpinBox.setMaximum(self._max_threads)
+        self.autoThreadBox.setDisabled(True)
+        
         self.set_manual_thread_message()
+        self._set_objective_variables()
         
         self.importButton.clicked.connect(self._import_config)
         self.exportButton.clicked.connect(self._export_config)
-        
         self.workdirLineEdit.returnPressed.connect(self._update_worker_dir)
         self.workdirToolButton.clicked.connect(self._select_worker_dir)
-        
         self.nThreadSpinBox.valueChanged.connect(self._update_n_threads)
+        self.penaltyDoubleSpinBox.valueChanged.connect(
+                                                    self._update_base_penalty)
+        self.cleanDirCheckBox.stateChanged.connect(
+                                            self._update_clean_existing_dir)
+        self.abortSpinBox.valueChanged.connect(self._update_max_simulations)
+        self._shell.project.sims_updated.connect(self._update_status)
         
-        self._update_status()
+        self._update_status(init=True)
         
+        return
+    
+    def _set_objective_variables(self):
+        
+        objective_variable = "project.lcoe_mode"
+        var_meta = self._shell.core.get_metadata(objective_variable)
+        var_text = var_meta.title
+        
+        self.costVarComboBox.addItem(var_text)
+        
+        if not var_meta.units:
+            self.penaltyUnitsLabel.clear()
+        else:
+            label_str = "({})".format(var_meta.units[0])
+            self.penaltyUnitsLabel.setText(label_str)
         
         return
     
     def set_manual_thread_message(self):
         
-        thread_msg = "(maximum of {} threads available)".format(
-                                                        self._max_threads)
+        thread_msg = "({} threads available)".format(self._max_threads)
         self.threadInfoLabel.setText(thread_msg)
         
         return
     
-    def _update_status(self):
+    @QtCore.pyqtSlot()
+    def _update_status(self, init=False):
         
-        (status_str,
-         status_code) = GUIAdvancedPosition.get_config_status(self._config)
+        color_map = {0: "#aa0000",
+                     1: "#00aa00"}
         
-        if status_code == 0:
-            str_color = "#aa0000"
-        elif status_code == 1:
-            str_color = "#00aa00"
+        (project_status_strs,
+         project_status_code) = GUIAdvancedPosition.get_project_status(
+                                                         self._shell.core,
+                                                         self._shell.project)
+        
+        (config_status_str,
+         config_status_code) = GUIAdvancedPosition.get_config_status(
+                                                                 self._config)
+        
+        status_template = '<li style="color: {};">{}</li>'
+        status_str = ""
+        
+        for project_status_str in project_status_strs:
+            status_str += \
+                    status_template.format(color_map[project_status_code],
+                                           project_status_str)
+        
+        status_str += status_template.format(color_map[config_status_code],
+                                             config_status_str)
+        
+        if self._config["worker_dir"] is not None:
+            
+            (worker_dir_status_strs,
+             worker_dir_status_codes) = \
+                 GUIAdvancedPosition.get_worker_directory_status(self._config)
+            
+            for (worker_dir_status_str,
+                 worker_dir_status_code) in zip(worker_dir_status_strs,
+                                                worker_dir_status_codes):
+                
+                status_str += \
+                    status_template.format(color_map[worker_dir_status_code],
+                                           worker_dir_status_str)
+        
+        # Define a global status
+        if (config_status_code == 0 or
+            project_status_code == 0 or
+            0 in worker_dir_status_codes):
+            
+            status_code = 0
+            
+        else:
+            
+            status_code = 1
         
         status_str_rich = ('<html><head/><body><p><span '
-                           'style=" font-size:11pt; color:{};">'
-                           '{}</span></p></body></html>').format(str_color,
-                                                                 status_str)
+                           'style="font-size: 10pt;">'
+                           '<ul>{}</ul>'
+                           '</span></p></body></html>').format(status_str)
         
         self.statusLabel.setText(status_str_rich)
         
@@ -154,25 +270,46 @@ class AdvancedPositionWidget(QtGui.QWidget,
         else:
             self.config_null.emit()
         
-        if self._config["worker_dir"] is not None:
-            self.workdirLineEdit.setText(self._config["worker_dir"])
-        
-        if self._config["n_threads"] is not None:
-            self.nThreadSpinBox.setValue(self._config["n_threads"])
-        
-        if self._config["threads_auto"]:
+        if init:
             
-            if not self.autoThreadBox.isChecked():
-                
-                self.autoThreadBox.toggle()
-                self.threadInfoLabel.setText("Auto mode")
-        
-        else:
+            if self._config["worker_dir"] is not None:
+                self.workdirLineEdit.setText(self._config["worker_dir"])
             
-            if self.autoThreadBox.isChecked():
+            if self._config["n_threads"] is not None:
+                self.nThreadSpinBox.setValue(self._config["n_threads"])
+            
+            if self._config["base_penalty"] is not None:
+                self.penaltyDoubleSpinBox.setValue(
+                                                self._config["base_penalty"])
+            
+            if self._config["clean_existing_dir"] is not None:
                 
-                self.autoThreadBox.toggle()
-                self.set_manual_thread_message()
+                if self._config["clean_existing_dir"]:
+                    
+                    if not self.cleanDirCheckBox.isChecked():
+                        self.cleanDirCheckBox.toggle()
+                
+                else:
+                    
+                    if self.cleanDirCheckBox.isChecked():
+                        self.cleanDirCheckBox.toggle()
+            
+            if self._config["max_simulations"] is not None:
+                self.abortSpinBox.setValue(self._config["max_simulations"])
+            
+            if self._config["threads_auto"]:
+                
+                if not self.autoThreadBox.isChecked():
+                    
+                    self.autoThreadBox.toggle()
+                    self.threadInfoLabel.setText("Auto mode")
+        
+            else:
+                
+                if self.autoThreadBox.isChecked():
+                    
+                    self.autoThreadBox.toggle()
+                    self.set_manual_thread_message()
         
         return
     
@@ -192,7 +329,7 @@ class AdvancedPositionWidget(QtGui.QWidget,
         config = GUIAdvancedPosition.load_config(file_path)
         self._config = self._init_config(config)
         
-        self._update_status()
+        self._update_status(init=True)
         
         return
     
@@ -250,9 +387,36 @@ class AdvancedPositionWidget(QtGui.QWidget,
         else:
             self._config["n_threads"] = None
         
-        print self._config["n_threads"]
+        self._update_status()
+        
+        return
+    
+    @QtCore.pyqtSlot(float)
+    def _update_base_penalty(self, penalty_value):
+        
+        self._config["base_penalty"] = penalty_value
+        
+        return
+    
+    @QtCore.pyqtSlot(object)
+    def _update_clean_existing_dir(self, checked_state):
+        
+        if checked_state == QtCore.Qt.Checked:
+            self._config["clean_existing_dir"] = True
+        else:
+            self._config["clean_existing_dir"] = None
         
         self._update_status()
+        
+        return
+    
+    @QtCore.pyqtSlot(int)
+    def _update_max_simulations(self, max_simulations):
+        
+        if max_simulations > 0:
+            self._config["max_simulations"] = max_simulations
+        else:
+            self._config["max_simulations"] = None
         
         return
     
